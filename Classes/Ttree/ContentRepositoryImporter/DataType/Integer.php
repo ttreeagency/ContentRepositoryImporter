bd613e09f940d0185d85dd7300f1d1a9286fd8e3
Dominique Feyer <dfeyer@ttree.ch>
2015-03-09 14:39:31 +0100

    [TASK] Introduce a new DataType for cleaning integer

---
+++ Classes/Ttree/ContentRepositoryImporter/DataType/Integer.php
@@ -0,0 +1,23 @@
+<?php
+namespace Ttree\ContentRepositoryImporter\DataType;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Ttree.ArchitectesCh".   *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * String Data Type
+ */
+class Integer extends String {
+
+	/**
+	 * @return integer
+	 */
+	public function getValue() {
+		$this->initializeValue($this->rawValue);
+		return (integer)$this->value;
+	}
+
+}
